67f739352e5871df2923d0043804c6cd1b21edd3
prabhuk <prabhu.trex@gmail.com>
2014-09-28 22:29:29 +0530

    Assignment: Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,55 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCacheMatrix function allows to create a matrix, populating and retrieving the values for the given matrix. 
 
-makeCacheMatrix <- function(x = matrix()) {
+## x = matrix() creates a matrix variable that is available in the makeCacheMatrix Context and not in the Global Context
 
+makeCacheMatrix <- function(x = matrix())
+{
+        ## matrixCache is the cache name. holds the cache value.
+        matrixCache <- NULL
+        
+        ## Function stores the give nmatrix.
+        setMatrix <- function(newMatrixValue) 
+        {
+                x <<- newMatrixValue
+                ## Clear the cache since a new matrix value has been assigned.
+                matrixCache <<- NULL
+        }
+        
+        ## retrieve the matrix that has been stored
+        getMatrix <- function() x
+        
+        ## cache the given matrix
+        setInverse <- function(solve) matrixCache <<- solve
+        
+        ## retrieve the matrix
+        getInverse <- function() matrixCache
+        
+        list(setMatrix = setMatrix,
+             getMatrix = getMatrix,
+             setInverse = setInverse,
+             getInverse = getInverse)
+        
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+## Inverse the matrix created by the last function and return the same
+cacheSolve <- function(x, ...)
+{
         ## Return a matrix that is the inverse of 'x'
+        
+        ## return the cached value if it exists
+        if(!is.null(x$getInverse()))
+                return (x$getInverse())
+        
+        ## Given the matrix, calculate its inverse 
+        inverse <- solve(x$getMatrix())
+        
+        ##  cache the inverse
+        x$setInverse(inverse)
+        
+        inverse
 }
